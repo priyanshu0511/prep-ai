cc91edd5445a4171fcdfe26f8657ad7773cfd209
priyanshu05112 <priyanshu05112@gmail.com>
2025-10-31 22:38:44 +0530

    changed sub-heading

--- src/app/dashboard/questions/page.jsx
+++ src/app/dashboard/questions/page.jsx
@@ -6,7 +6,7 @@ const InterviewPage = () => {
   return (
      <div className='p-10'>
       <h2 className='font-bold text-2xl'>Dashboard</h2>
-      <h2 className='text-gray-500'>Create and start your AI Mockup Interview</h2>
+      <h2 className='text-gray-500'>Start practicing coding questions right now!</h2>
 
       <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
         <AddNewQuestion />
