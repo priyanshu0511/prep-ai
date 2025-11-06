7f98d4448e39a583dbf0903630650ad7cb06c7bc
priyanshu05112 <priyanshu05112@gmail.com>
2025-11-06 23:27:49 +0530

    added coming soon for cover letter and insights

---
+++ src/app/dashboard/insights/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PerformanceAnalyticsPage = () => {
+  return (
+    <div className="text-2xl flex justify-center items-center h-screen text-foreground">Coming Soon...</div>
+  )
+}
+
+export default PerformanceAnalyticsPage
